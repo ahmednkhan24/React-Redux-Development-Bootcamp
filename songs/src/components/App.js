0584aa5ea236dbde9e43b19ce5011ba1da9ba8bd
ahmednkhan24 <ahmednkhan24@gmail.com>
2019-05-30 02:20:56 -0500

    songs: created template

---
+++ songs/src/components/App.js
@@ -0,0 +1,16 @@
+/*
+ * Created as a functional component as opposed to class based component because
+ * now that we are making use of redux we're going to make use 
+ * of component level state much less frequently.
+ * Instead we're going to generally store all of our data inside of redux instead.
+ */
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>App</div>
+    );
+};
+
+export default App;
